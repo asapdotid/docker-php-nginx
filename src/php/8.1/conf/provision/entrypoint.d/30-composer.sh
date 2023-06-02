059dfe8d371f85c7b9c3135524202f79ad24603c
Asapdotid <asapdotid@gmail.com>
2023-06-02 15:04:12 +0700

    ⚡️ IMPROVE: change function checking for install package composer

--- src/php/8.1/conf/provision/entrypoint.d/30-composer.sh
+++ src/php/8.1/conf/provision/entrypoint.d/30-composer.sh
@@ -1,7 +1,7 @@
 # Composer install
 if [ "$SKIP_COMPOSER" != true ]; then
     # Try auto install for composer
-    if [[ -f "$CONTAINER_WORKDIR_PATH/composer.json" && -f "$CONTAINER_WORKDIR_PATH/composer.lock" ]]; then
+    if [ -f "$CONTAINER_WORKDIR_PATH/composer.json" ]; then
         if [ "$APPLICATION_ENV" == "development" ]; then
             composer install --working-dir=$CONTAINER_WORKDIR_PATH
         else
